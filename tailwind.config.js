6163e9dfaed7bb20f3214d30c51796e0a4c41840
AkashSDas <aakashdas368@gmail.com>
2022-08-01 09:24:26 +0530

    setup tailwind css design system

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,22 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      grey: "hsla(0, 0%, 46%, 1)",
+      black: "hsla(0, 0%, 10%, 1)",
+      white: "hsla(0, 0%, 100%, 1)",
+      violet: "hsla(259, 100%, 53%, 1)",
+      "dark-violet": "hsla(259, 100%, 22%, 1)",
+      "light-violet": "hsla(241, 68%, 95%, 1)",
+      "light-purple": "hsla(256, 73%, 97%, 1)",
+    },
+    boxShadow: {
+      default: "0px 4px 8px 0px hsla(0, 0%, 0%, 0.2)",
+    },
+    // fontSize: {},
+    fontFamily: {
+      poppins: "Poppins, sans-serif",
+    },
     extend: {},
   },
   plugins: [],
