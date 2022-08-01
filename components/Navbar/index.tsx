9d67f14fe34f2b125e70c002f5c9b1e7c1b90c39
AkashSDas <aakashdas368@gmail.com>
2022-08-01 14:01:43 +0530

    refactor navbar and create new courses dropdown

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,32 +1,22 @@
+import { useState } from "react";
+
 import CoursesDropDown from "@components/DropDown/CoursesDropDown";
+import CoursesDropDownMenu from "@components/DropDown/CoursesDropDownMenu";
 import ProductsDropDown from "@components/DropDown/ProductsDropDown";
 import { ArrowDropDown } from "@components/Icons/ArrowDropDown";
 import styles from "@styles/components/Navbar.module.scss";
 
+import NavItem from "./NavItem";
+
 export default function Navbar() {
   return (
     <nav className={styles.navbar}>
-      <div className={styles["navbar-child"]}>
+      <ul className={styles["nav-left"]}>
         <img src="/logo.svg" alt="iNeuron Logo" />
-        <CoursesDropDown label="Courses" />
-        <ProductsDropDown />
-        <DropDown label="Company" />
-      </div>
-
-      <div className={styles["navbar-child"]}>
-        <div>Search</div>
-        <button>Login</button>
-        <button>Get Started</button>
-      </div>
+        <NavItem label="Courses">
+          <CoursesDropDownMenu />
+        </NavItem>
+      </ul>
     </nav>
-  );
-}
-
-function DropDown({ label }) {
-  return (
-    <div className="cursor-pointer text-grey hover:text-black flex items-center gap-[0.625rem] group">
-      {label}{" "}
-      <ArrowDropDown className="stroke-grey group-hover:stroke-black group-hover:rotate-180 transition-all duration-300" />
-    </div>
   );
 }
