9bd7d1dab560293d9a526484c91f3eb475c45dac
AkashSDas <aakashdas368@gmail.com>
2022-08-01 16:01:35 +0530

    create a hero section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,16 @@
+import PrimaryButton from "@components/Button/PrimaryButton";
+import { Hero } from "@components/Hero/Hero";
+import { ArrowRight2 } from "@components/Icons/ArrowRight2";
 import Navbar from "@components/Navbar/index";
 
 export default function HomePage() {
   return (
     <>
       <Navbar />
+
+      <main className="mt-[150px] flex flex-col gap-[150px]">
+        <Hero />
+      </main>
     </>
   );
 }
