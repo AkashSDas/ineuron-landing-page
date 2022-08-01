ffe6006ae4eb210c01453e03293d119cf8eec935
AkashSDas <aakashdas368@gmail.com>
2022-08-01 12:35:52 +0530

    create arrow right icon

---
+++ components/Icons/ArrowRight.tsx
@@ -0,0 +1,19 @@
+export function ArrowRight({ className }) {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <path
+        d="M10 7L14 12L10 17"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
