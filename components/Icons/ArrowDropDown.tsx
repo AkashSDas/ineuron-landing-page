2b8ed0ad408d2a92f308d4ad8488a024bddf0103
AkashSDas <aakashdas368@gmail.com>
2022-08-01 11:30:25 +0530

    create ArrowDropDown icon component

---
+++ components/Icons/ArrowDropDown.tsx
@@ -0,0 +1,19 @@
+export function ArrowDropDown({ className }) {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <path
+        d="M7 10L12 14L17 10"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
