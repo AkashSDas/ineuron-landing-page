07f1e0c6e44d075156e9ecff2128d9acc4701d3a
AkashSDas <aakashdas368@gmail.com>
2022-08-02 08:37:54 +0530

    create instructors section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import PrimaryButton from "@components/Button/PrimaryButton";
 import { Hero } from "@components/Hero/Hero";
 import { ArrowRight2 } from "@components/Icons/ArrowRight2";
+import { InstructorSection } from "@components/InstructorsSection";
 import Navbar from "@components/Navbar/index";
 import { Programs } from "@components/Programs/index";
 
@@ -12,6 +13,7 @@ export default function HomePage() {
       <main className="mt-[150px] flex flex-col gap-[150px]">
         <Hero />
         <Programs />
+        <InstructorSection />
       </main>
     </>
   );
