115e68f1b819486be6d4e8b8fe9da6f57d875e7b
AkashSDas <aakashdas368@gmail.com>
2022-08-02 23:38:41 +0530

    animate navbar

--- components/Header/LongNavbar.tsx
+++ components/Header/LongNavbar.tsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion";
+
 import PrimaryButton from "@components/Button/PrimaryButton";
 import SearchButton from "@components/Button/SearchButton";
 import TextButton from "@components/Button/TextButton";
@@ -26,20 +28,56 @@ function LongNavbar() {
     "Terms & Conditions",
   ];
 
+  const container = {
+    animate: {
+      transition: { delayChildren: 0.4, staggerChildren: 0.1 },
+    },
+  };
+
+  const item = {
+    initial: { y: -20, opacity: 0 },
+    animate: {
+      y: 0,
+      opacity: 1,
+      transition: { ease: [0.6, 0.01, -0.05, 0.95], duration: 1 },
+    },
+  };
+
   return (
     <nav className={styles.navbar}>
-      <ul className={styles["nav-left"]}>
-        <img src="/logo.svg" alt="iNeuron Logo" />
-        <CoursesDropDown />
-        <DropDown label="Products" items={products} />
-        <DropDown label="Company" items={companyItems} />
-      </ul>
+      <motion.ul
+        variants={container}
+        initial="initial"
+        animate="animate"
+        className={styles["nav-left"]}
+      >
+        <motion.img variants={item} src="/logo.svg" alt="iNeuron Logo" />
+        <motion.div variants={item}>{<CoursesDropDown />}</motion.div>
+        <motion.div variants={item}>
+          <DropDown label="Products" items={products} />
+        </motion.div>
+        <motion.div variants={item}>
+          <DropDown label="Company" items={companyItems} />
+        </motion.div>
+      </motion.ul>
 
-      <ul className={styles["nav-right"]}>
-        <SearchButton />
-        <TextButton label="Login" />
-        <PrimaryButton label="Get Started" />
-      </ul>
+      <motion.ul
+        variants={container}
+        initial="initial"
+        animate="animate"
+        transition={{ delay: 1.6 }}
+        className={styles["nav-right"]}
+      >
+        <motion.div variants={item}>
+          <SearchButton />
+        </motion.div>
+        <motion.div variants={item}>
+          <TextButton label="Login" />
+        </motion.div>
+        <motion.div variants={item}>
+          <PrimaryButton label="Get Started" />
+        </motion.div>
+      </motion.ul>
     </nav>
   );
 }
