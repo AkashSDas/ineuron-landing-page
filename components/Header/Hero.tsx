9cdac197ac8c9ffc4fda2e53d0e144a3933f558e
AkashSDas <aakashdas368@gmail.com>
2022-08-02 23:46:16 +0530

    add hero animation

--- components/Header/Hero.tsx
+++ components/Header/Hero.tsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion";
+
 import IconFilledButton from "@components/Button/IconFilledButton";
 import { ArrowRight2 } from "@components/Icons/ArrowRight2";
 import styles from "@styles/components/Header/Hero.module.scss";
@@ -5,20 +7,57 @@ import styles from "@styles/components/Header/Hero.module.scss";
 function Hero() {
   return (
     <section className={styles["hero-wrapper"]}>
-      <h1 className={styles.heading}>
+      <motion.h1
+        className={styles.heading}
+        initial={{ y: 40, opacity: 0 }}
+        animate={{
+          y: 0,
+          opacity: 1,
+          transition: {
+            ease: [0.6, 0.01, -0.05, 0.95],
+            duration: 1,
+            delay: 1.2,
+          },
+        }}
+      >
         Premium <span className="text-stroke">quality</span> and <br />{" "}
         <span className="text-stroke">affordable</span> price
-      </h1>
+      </motion.h1>
 
-      <p className={styles.description}>
+      <motion.p
+        className={styles.description}
+        initial={{ y: 40, opacity: 0 }}
+        animate={{
+          y: 0,
+          opacity: 1,
+          transition: {
+            ease: [0.6, 0.01, -0.05, 0.95],
+            duration: 1,
+            delay: 1.4,
+          },
+        }}
+      >
         Learn modern technologies at Premium Quality from Industry Experts at
         Affordable Price
-      </p>
+      </motion.p>
 
-      <IconFilledButton
-        label="Get Started"
-        icon={<ArrowRight2 className="stroke-white" />}
-      />
+      <motion.div
+        initial={{ y: 40, opacity: 0 }}
+        animate={{
+          y: 0,
+          opacity: 1,
+          transition: {
+            ease: [0.6, 0.01, -0.05, 0.95],
+            duration: 1,
+            delay: 1.6,
+          },
+        }}
+      >
+        <IconFilledButton
+          label="Get Started"
+          icon={<ArrowRight2 className="stroke-white" />}
+        />
+      </motion.div>
     </section>
   );
 }
