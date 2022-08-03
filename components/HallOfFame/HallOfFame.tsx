f4a4eca5f2a1b4d6982cdbb8f71f0747ed59f0e8
AkashSDas <aakashdas368@gmail.com>
2022-08-03 21:45:01 +0530

    fix main content responsiveness

--- components/HallOfFame/HallOfFame.tsx
+++ components/HallOfFame/HallOfFame.tsx
@@ -13,7 +13,7 @@ function HallOfFame() {
     arrows: false,
     infinite: true,
     speed: 500,
-    slidesToShow: 2.75,
+    slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
