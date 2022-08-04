fb19a577ad0d6dcb2b5cb73d7e6dd0d23c2d084a
AkashSDas <aakashdas368@gmail.com>
2022-08-04 11:53:27 +0530

    fix fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "@styles/styles.scss";
 
+import Head from "next/head";
 import { useState } from "react";
 
 import { CursorProvider } from "@components/Cursor/CursorProvider";
@@ -10,14 +11,30 @@ export default function MyApp({ Component, pageProps }) {
   const [searchModalIsOpen, setSearchModalIsOpen] = useState(false);
 
   return (
-    <SmallNavbarContext.Provider value={{ isOpen, setIsOpen }}>
-      <SearchModalContext.Provider
-        value={{ searchModalIsOpen, setSearchModalIsOpen }}
-      >
-        <CursorProvider>
-          <Component {...pageProps} />
-        </CursorProvider>
-      </SearchModalContext.Provider>
-    </SmallNavbarContext.Provider>
+    <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css?family=Poppins:wght@300;400;500;600;700;800&display=swap"
+          rel="stylesheet"
+          type="text/css"
+        />
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📺</text></svg>"
+        />
+
+        <title>iNeuron</title>
+      </Head>
+
+      <SmallNavbarContext.Provider value={{ isOpen, setIsOpen }}>
+        <SearchModalContext.Provider
+          value={{ searchModalIsOpen, setSearchModalIsOpen }}
+        >
+          <CursorProvider>
+            <Component {...pageProps} />
+          </CursorProvider>
+        </SearchModalContext.Provider>
+      </SmallNavbarContext.Provider>
+    </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,3 @@
-import { AnimatePresence } from "framer-motion";
-import Head from "next/head";
 import { useContext, useEffect } from "react";
 
 import CoursesSlideShow from "@components/CoursesSlideShow/CoursesSlideShow";
@@ -32,20 +30,6 @@ export default function HomePage() {
 
   return (
     <div className="-visible">
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css?family=Poppins:wght@300;400;500;600;700;800&display=swap"
-          rel="stylesheet"
-          type="text/css"
-        />
-        <link
-          rel="icon"
-          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📺</text></svg>"
-        />
-
-        <title>iNeuron</title>
-      </Head>
-
       <LongNavbar />
       <SmallNavbar />
 
