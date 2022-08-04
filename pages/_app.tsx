4c3e213f6660a8443b06aa2b0f9934e3ac2e405e
AkashSDas <aakashdas368@gmail.com>
2022-08-04 12:09:26 +0530

    fix font issue

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,11 +14,6 @@ export default function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <link
-          href="https://fonts.googleapis.com/css?family=Poppins:wght@300;400;500;600;700;800&display=swap"
-          rel="stylesheet"
-          type="text/css"
-        />
-        <link
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📺</text></svg>"
         />
