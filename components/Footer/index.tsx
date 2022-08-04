0bbfabfcc66d40af6e41db900c2a4f258a3130ac
AkashSDas <aakashdas368@gmail.com>
2022-08-04 09:35:12 +0530

    refactor footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -21,22 +21,25 @@ function Footer() {
   );
 
   const company = [
-    "About Us",
-    "Contact Us",
-    "Hackathon",
-    "Job Guarantee",
-    "Job Assistance",
-    "FAQs",
-    "Privacy Policy",
-    "Terms & Conditions",
+    { label: "About Us", href: "https://ineuron.ai/about-us" },
+    { label: "Contact Us", href: "https://ineuron.ai/contact-us" },
+    { label: "Hackathon", href: "https://hackathon.ineuron.ai/neo" },
+    { label: "Job Guarantee", href: "https://ineuron.ai/job-guarantee" },
+    { label: "Job Assistance", href: "https://ineuron.ai/job-assistance" },
+    { label: "FAQs", href: "https://ineuron.ai/faqs" },
+    { label: "Privacy Policy", href: "https://ineuron.ai/privacy-policy" },
+    {
+      label: "Terms & Conditions",
+      href: "https://ineuron.ai/terms-and-conditions",
+    },
   ];
 
   const products = [
-    "Job Portal",
-    "Internship Portal",
-    "Become an Affiliate",
-    "Hall of Fame",
-    "InBlog",
+    { label: "Job Portal", href: "https://jobs.ineuron.ai/" },
+    { label: "Intership Portal", href: "https://internship.ineuron.ai/" },
+    { label: "Become an Affiliate", href: "https://affiliate.ineuron.ai/" },
+    { label: "Hall of Fame", href: "https://halloffame.ineuron.ai/" },
+    { label: "InBlog", href: "https://blog.ineuron.ai/" },
   ];
 
   return (
@@ -46,25 +49,44 @@ function Footer() {
           <img src="/logo.svg" alt="iNeuron Logo" />
 
           <div className="flex items-center gap-4">
-            <YouTube />
-            <Linkedin />
-            <Twitter />
-            <Facebook />
+            <a
+              href="https://www.youtube.com/channel/UCb1GdqUqArXMQ3RS86lqqOw"
+              target="_blank"
+            >
+              <YouTube />
+            </a>
+            <a href="https://www.facebook.com/ineuronai" target="_blank">
+              <Facebook />
+            </a>
+            <a href="https://twitter.com/iNeuronAi" target="_blank">
+              <Twitter />
+            </a>
+            <a
+              href="https://www.linkedin.com/company/ineuron-ai/"
+              target="_blank"
+            >
+              <Linkedin />
+            </a>
           </div>
         </div>
 
         <div className="flex gap-2">
           <Mail />{" "}
           <span className="text-[15px] text-grey">
-            Email us: contact@ineuron.ai
+            <a href="mailto:contact@ineuron.ai">Email us: contact@ineuron.ai</a>
           </span>
         </div>
 
         <div className="flex gap-2">
           <Location />{" "}
           <span className="text-[15px] text-grey w-[50%]">
-            17th Floor Tower A, Brigade Signature Towers, Sannatammanahalli,
-            Bengaluru, Karnataka 562129.
+            <a
+              href="https://www.google.com/maps/place/iNeuron.ai/@13.0542869,77.7624757,15z/data=!4m5!3m4!1s0x0:0x89fbcd434743b11b!8m2!3d13.0540489!4d77.7614087"
+              target="_blank"
+            >
+              17th Floor Tower A, Brigade Signature Towers, Sannatammanahalli,
+              Bengaluru, Karnataka 562129.
+            </a>
           </span>
         </div>
 
@@ -79,9 +101,11 @@ function Footer() {
           <Roller2 label="Company" />
         </li>
 
-        {company.map((label) => (
-          <li key={label} className={styles.li}>
-            <Roller label={label} />
+        {company.map((item) => (
+          <li key={item.label} className={styles.li}>
+            <a href={item.href} target="_blank">
+              <Roller label={item.label} />
+            </a>
           </li>
         ))}
       </ul>
@@ -91,9 +115,11 @@ function Footer() {
           <Roller2 label="Products" />
         </li>
 
-        {products.map((label) => (
-          <li key={label} className={styles.li}>
-            <Roller label={label} />
+        {products.map((item) => (
+          <li key={item.label} className={styles.li}>
+            <a href={item.href} target="_blank">
+              <Roller label={item.label} />
+            </a>
           </li>
         ))}
       </ul>
